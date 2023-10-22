a7de16c5f06da4f460a0d50340ac9271513635be
Dev : MOHAMED KAMAL <MOHAMED.KAMAL18@outlook.com>
2023-10-22 21:15:13 +0300

    Edit 5

--- Assessment/wwwroot/js/customersDatatable.js
+++ Assessment/wwwroot/js/customersDatatable.js
@@ -1,26 +1,3 @@
 ﻿$(document).ready(function () {
-    $('#example').dataTable({
-        "processing": true,
-        "serverSide": false,
-        "filter": true,
-        "columnDefs": [{
-            "targets": [0],
-            "visible": false,
-            "searchable": false
-        }],
-        "columns": [
-            { "data": "ID", "name": "ID", "autowidth": true },
-            { "data": "Name", "name": "Name", "autowidth": true },
-            { "data": "Department", "name": "Department", "autowidth": true },
-            { "data": "Phone", "name": "Phone", "autowidth": true },
-            { "data": "Email", "name": "Email", "autowidth": true },
-            { "data": "Date_Created", "name": "Date_Created", "autowidth": true },
-            { "data": "Created_By", "name": "Created_By", "autowidth": true },
-            { "data": "Status", "name": "Status", "autowidth": true },
-            {
-                "render": function (data, type, row) { return '<a href="#" class="btn btn-danger" onclick=DeleteCustomer("' + row.ID + '"); > Delete </a>'},
-                "orderable": false
-            },
-        ]
-    });
+    $('#example').dataTable({});
 });
