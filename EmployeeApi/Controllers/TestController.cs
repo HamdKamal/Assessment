5d3da16f919f7fd75f83f07a4d2b84a94415d0c1
Dev : MOHAMED KAMAL <MOHAMED.KAMAL18@outlook.com>
2023-10-25 00:40:38 +0300

    Api - Test

---
+++ EmployeeApi/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Core.Interfaces;
+using Core.ViewModel;
+using Databases.Data;
+using Databases.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly DatabaseDbContext _db;
+        private readonly IEmployee _employee;
+
+        public TestController(DatabaseDbContext context,IEmployee employee) 
+        { 
+          _db = context;
+          _employee = employee;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<EmployeeVM>> GetEmployeeList()
+        {
+            var result = await _employee.GetAll();
+            return result.ToArray();
+        }
+    }
+}
---
+++ EmployeeApi/Startup.cs
@@ -0,0 +1,106 @@
+﻿using Core.Interfaces;
+using Core.Repository;
+using Databases.Data;
+using Databases.Models.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace EmployeeApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseDbContext>(options =>
+              options.UseSqlServer(
+                  Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<Users, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 6;
+                config.Password.RequireDigit = false;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = false;
+                config.Password.RequireLowercase = false;
+            })
+          .AddEntityFrameworkStores<DatabaseDbContext>()
+          .AddDefaultTokenProviders();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Issuer"],
+                    ValidAudience = Configuration["AudienceId"],
+                    IssuerSigningKey = new
+                    SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes
+                    (Configuration["AudienceSecret"]))
+                };
+            });
+
+            services.AddMvc();
+            services.AddControllersWithViews();
+            services.AddControllersWithViews();
+            services.AddScoped<IEmployee, EmployeeRepo>();
+
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EmployeeApi", Version = "v1" });
+            });
+
+            services.AddRazorPages();
+            services.AddHealthChecks();
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                 name: "default",
+                 pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
